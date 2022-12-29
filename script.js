f0efb1f435a0ef847f9e7f285df8dadc14a4935f
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-12-29 23:21:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,218 @@
+console.log('hello Dev guild');
+// var name1 = '5';
+// var x = 5;
+// var y = 10;
+// var z;
+
+// defining what variables are
+// let p = 15;
+// let b = 10;
+
+// console.log(x > y);
+// console.log(x < y);
+// console.log( x == y )
+
+
+// console.log( x != y )
+
+
+// console.log( x >= y )
+// console.log( x <= y )
+
+
+// console.log( x === name1 )
+
+// console.log(typeof(name1))
+// console.log(typeof(x))
+// console.log(typeof(z))
+
+
+
+// console.log(typeof(x))
+
+
+//TODAY 26TH NOV 2022.
+
+// https://www.w3schools.com/js/js_arithmetic.asp
+// Javascript Arithmetic 
+// JavaScript Arithmetic Operators
+// Addition +
+// Subtraction -
+// Multiplication *
+// Exponentiation **
+// Division /
+// Modulus %
+// Increment ++
+// Decrement --
+
+// vocabularies
+// Operators and Operands
+
+// As variable
+// let x = 100 + 50;
+// console.log(x)
+
+// Expression
+// let y = (100 + 50) * a;
+// console.log(y)
+
+// Adding 
+// let a = 5;
+// let b = 2;
+// let c = a + b;
+
+// Subtracting
+// let p = 5;
+// let b = 2;
+// let z = p - b;
+
+// Dividing
+// let j = 5;
+// let k = 2;
+// let q = x / y;
+
+//Remainder
+// let n = 5;
+// let m = 2;
+// let o = n % m;
+
+// Exponenntiation
+// let v = 5;
+// let z = v ** 2;
+// produce thesame result has 
+// Math.pow(v, 2)
+
+// Operation precedence
+// BODMAS
+// Bracket => Of => Division => Multiplication => Addition => Subtraction
+
+// DECEMBER 3 2022
+// JAVASCRIPT DATA TYPES
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 5. Null
+// 7. Symbol
+// 8. Object
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+
+// Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// Booleans
+// let x = true;
+// let y = false;
+
+// Object:
+// const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+// const date = new Date("2022-03-25");
+
+// let x = 16 + "Volvo";
+// let x = "16" + "Volvo";
+
+// let x;       // Now x is undefined
+// x = 5;       // Now x is a Number
+// x = "John";  // Now x is a String
+
+// STRINGS
+// Using double quotes:
+// let carName1 = "Volvo XC60";
+
+// Using single quotes:
+// let carName2 = 'Volvo XC60';
+
+// NUMBER
+// Using double quotes:
+// let carName1 = "Volvo XC60";
+
+// Using single quotes:
+// let carName2 = 'Volvo XC60';
+
+//BOOLEAN
+// let x = 5;
+// let y = 5;
+// let z = 6;
+// (x == y)       // Returns true
+// (x == z)       // Returns false
+
+//OBJECT
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+//TYPEOF OPERATOR
+// typeof ""             // Returns "string"
+// typeof "John"         // Returns "string"
+// typeof 0              // Returns "number"
+// typeof 314            // Returns "number"
+// typeof 3.14           // Returns "number"
+
+//UNDEFINED
+// let car;    // Value is undefined, type is undefined
+
+
+
+
+// DECEMBER 17 2022
+
+//JAVASCRIPT FUNCTIONS
+// write a function that adds two numbers together.
+
+// function addTwoNumbers(num1, num2){
+//     // console.log( num1 + num2)
+//     return num1 + num2;
+
+// }
+
+
+
+// let sum = addTwoNumbers(8, 14)
+
+var nameOfGuild = "dev guild"
+var numberOfTimes = 3;
+var isOnline = false;
+
+
+// addTwoNumbers(2, 14)
+// addTwoNumbers(5, 20)
+// addTwoNumbers(65, 14)
+// addTwoNumbers(80, 27)
+// addTwoNumbers(57, 27)
+
+// converts fahrenheit to celsius
+function toCelsius(fehrenheit){
+    // console.log("your temp in celcius is")
+    // console.log( (5/9) * (fehrenheit - 32));
+    return ( (5/9) * (fehrenheit - 32));
+}
+
+// toCelsius(100);
+
+// console.log(typeof nameOfGuild)
+// console.log(typeof numberOfTimes)
+// console.log(typeof isOnline)
+
+// let tempC = toCelsius(100)
+
+function myFunction() {
+    let nameOfCar = "Benz"
+    console.log(nameOfCar)
+    console.log(nameOfGuild)
+}
+
+
+myFunction()
+// console.log(nameOfCar)
+// console.log(nameOfGuild)
+
+// wrirte afunction that take in a two parameters, which are a name(NAME) and an age(AGE): and returns the statement 
+// "welcome NAME, your age is AGE"
