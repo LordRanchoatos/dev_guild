03638e31dc8c69a5ac649c3bf075907a0f2605ab
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-01-07 21:26:11 +0100

    class end

--- script.js
+++ script.js
@@ -12,13 +12,10 @@ console.log('hello Dev guild');
 // console.log(x < y);
 // console.log( x == y )
 
-
 // console.log( x != y )
-
 
 // console.log( x >= y )
 // console.log( x <= y )
-
 
 // console.log( x === name1 )
 
@@ -26,15 +23,12 @@ console.log('hello Dev guild');
 // console.log(typeof(x))
 // console.log(typeof(z))
 
-
-
 // console.log(typeof(x))
-
 
 //TODAY 26TH NOV 2022.
 
 // https://www.w3schools.com/js/js_arithmetic.asp
-// Javascript Arithmetic 
+// Javascript Arithmetic
 // JavaScript Arithmetic Operators
 // Addition +
 // Subtraction -
@@ -56,7 +50,7 @@ console.log('hello Dev guild');
 // let y = (100 + 50) * a;
 // console.log(y)
 
-// Adding 
+// Adding
 // let a = 5;
 // let b = 2;
 // let c = a + b;
@@ -79,7 +73,7 @@ console.log('hello Dev guild');
 // Exponenntiation
 // let v = 5;
 // let z = v ** 2;
-// produce thesame result has 
+// produce thesame result has
 // Math.pow(v, 2)
 
 // Operation precedence
@@ -159,9 +153,6 @@ console.log('hello Dev guild');
 //UNDEFINED
 // let car;    // Value is undefined, type is undefined
 
-
-
-
 // DECEMBER 17 2022
 
 //JAVASCRIPT FUNCTIONS
@@ -173,14 +164,11 @@ console.log('hello Dev guild');
 
 // }
 
-
-
 // let sum = addTwoNumbers(8, 14)
 
 // var nameOfGuild = "dev guild"
 // var numberOfTimes = 3;
 // var isOnline = false;
-
 
 // addTwoNumbers(2, 14)
 // addTwoNumbers(5, 20)
@@ -189,10 +177,10 @@ console.log('hello Dev guild');
 // addTwoNumbers(57, 27)
 
 // converts fahrenheit to celsius
-function toCelsius(fehrenheit){
-    // console.log("your temp in celcius is")
-    // console.log( (5/9) * (fehrenheit - 32));
-    // return ( (5/9) * (fehrenheit - 32));
+function toCelsius(fehrenheit) {
+  // console.log("your temp in celcius is")
+  // console.log( (5/9) * (fehrenheit - 32));
+  // return ( (5/9) * (fehrenheit - 32));
 }
 
 // toCelsius(100);
@@ -209,14 +197,41 @@ function toCelsius(fehrenheit){
 //     console.log(nameOfGuild)
 // }
 
-
 // myFunction()
 // console.log(nameOfCar)
 // console.log(nameOfGuild)
 
-// wrirte afunction that take in a two parameters, which are a name(NAME) and an age(AGE): and returns the statement 
+// wrirte afunction that take in a two parameters, which are a name(NAME) and an age(AGE): and returns the statement
 // "welcome NAME, your age is AGE"
-
 
 //  DATE: 07/01/22
 // TOPIC JAVASCRIPT OBJECTS
+const car = {
+  name: 'BMW',
+  year: 2022,
+  color: 'white',
+  doors: 4,
+  honk: function () {
+    return this.name + ' honk!!!';
+  },
+};
+
+class Person {
+  constructor(firstname, lastname) {
+    this.fistname = firstname;
+    this.lastname = lastname;
+  }
+  greet() {
+    console.log(this.fistname + ' greetings!!');
+  }
+}
+
+let Lord = new Person('Dave', 'King');
+Lord.greet();
+
+let attendees = ['AGI', 'Lord', 'jaux', 'spiritedF'];
+
+attendees.forEach((attendefirstName) => {
+  let attendee = new Person(attendefirstName, '');
+  attendee.greet();
+});
